9e46a44ced3966919b411ade8f5707b6106ceb4a
Arun R Variyath <32630496+arunvariyath@users.noreply.github.com>
2022-01-28 00:45:21 +0530

    Added Gallery and Poems placeholders

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,8 @@
-(function() {
-	"use strict";
-/**
-   * Easy selector helper function
-   */
+(function () {
+  "use strict";
+  /**
+     * Easy selector helper function
+     */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -11,24 +11,38 @@
       return document.querySelector(el)
     }
   }
-function submitContactMe()
-{
-	alert('Submitted!')
-	window.location.href="/home"
-}
-/**
-   * Hero type effect
-   */
- const typed = select('.typed')
- if (typed) {
-   let typed_strings = typed.getAttribute('data-typed-items')
-   typed_strings = typed_strings.split(',')
-   new Typed('.typed', {
-	 strings: typed_strings,
-	 loop: true,
-	 typeSpeed: 100,
-	 backSpeed: 50,
-	 backDelay: 2000
-   });
- }
+
+  /**
+     * Hero type effect
+     */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  const ageSpan = select('#ageSpan')
+  const dobSpan = select('#dobSpan')
+  if (ageSpan && dobSpan) {
+    let dob = dobSpan.textContent
+    var mydate = new Date(dob);
+    ageSpan.textContent = _calculateAge(mydate);
+  }
+
 })()
+function submitContactMe() {
+  alert('Submitted!')
+  window.location.href = "/home"
+}
+function _calculateAge(birthday) { // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,75 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Home',other: true})
+  res.render('index', { title: 'Home', other: true })
 });
 /* GET home page. */
 router.get('/home', function (req, res, next) {
-  res.render('index', { title: 'Home',other: true})
+  res.render('index', { title: 'Home', other: true })
 });
 
 /* GET Gallery page. */
 router.get('/gallery', function (req, res, next) {
-  res.render('gallery', { title: 'Gallery',other: true})
+  let galleryImages = [{
+    imageSrc: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
+    imageTitle: 'some image',
+    imageDescription: 'some description'
+  }, {
+    imageSrc: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
+    imageTitle: 'some image',
+    imageDescription: 'some description'
+  }, {
+    imageSrc: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
+    imageTitle: 'some image',
+    imageDescription: 'some description'
+  }, {
+    imageSrc: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
+    imageTitle: 'some image',
+    imageDescription: 'some description'
+  }]
+  res.render('gallery', { title: 'Gallery', other: true,galleryImages })
 });
 
-/* GET Contact page. */
-router.get('/contact', function (req, res, next) {
-  res.render('contact', { title: 'Contact Me',other: true})
+/* GET message page. */
+router.get('/message', function (req, res, next) {
+  res.render('message', { title: 'Contact Me', other: true })
 });
+/* GET Contact page. */
+router.get('/view-messages', function (req, res, next) {
+  res.render('view-messages', { title: 'Contact Me', other: true })
+});
+
 
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'About Me',other: true})
+  res.render('about', { title: 'About Me', other: true })
 });
 
+var poems = [{
+  poemSrc: 'h00fHCy1Uio',
+  poemTitle: 'Title Sample',
+  poemLyrics: 'fsahfsahfgssakjrrfhljsafhljfsahfsahfgssa kjrrfhljsafhljfsahfsahfgssakjrrfhljsafhljfsahfsahfgssakjrrfhlj safhljfsahfsahfgssakjrrfhljsafhljfsahfsahfg ssakjrrfhljsafhljfsahfsahfgssakjrrfhljsafhljfsahfsahfgssakjrrfhljsafhljfsahfsahfgssakjrrfhljsafhlj'
 
-
+}, {
+  poemSrc: '65SCxJV28Kk', poemTitle: 'Title Sample'
+}, {
+  poemSrc: 'Z9Nj6D3rEq8'
+}, {
+  poemSrc: '-TBb9FWYLmw'
+}, {
+  poemSrc: 'x4JuuqBGm6Y'
+}, {
+  poemSrc: '65SCxJV28Kk'
+}, {
+  poemSrc: 'Z9Nj6D3rEq8'
+}, {
+  poemSrc: '-TBb9FWYLmw'
+}, {
+  poemSrc: 'x4JuuqBGm6Y'
+}]
 /* GET about page. */
 router.get('/poems', function (req, res, next) {
-  res.render('poems', { title: 'Poems',other: true})
+  res.render('poems', { title: 'Poems', other: true, poems })
 });
 
 var popup = {
