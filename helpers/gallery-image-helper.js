d9020cfed62b5b037ec0da87cd438f51dad278d9
Arun R Variyath <arvforstudies@gmail.com>
2022-02-05 08:50:29 +0530

    Added database connection to member details- but failing parsing data from db

---
+++ helpers/gallery-image-helper.js
@@ -0,0 +1,10 @@
+var connection = require("../config/connection");
+
+module.exports = {
+  getAllMembers() {
+    return new Promise(async (resolve, reject) => {
+      let members = await connection.get().collection("family-members").find().toArray();
+      resolve(members)
+    });
+  },
+};
